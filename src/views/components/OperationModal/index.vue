26e6395336f4422995ab03037695d23c5aa25f16
muhammadp <muhammad.perreira@onfinality.io>
2022-09-07 17:16:20 -0400

    revert

--- src/views/components/OperationModal/index.vue
+++ src/views/components/OperationModal/index.vue
@@ -325,7 +325,7 @@ export default {
         return this.address
       }
       if (this.accounts) {
-        const chain = 'Point'
+        const chain = "this.$store.state.chains.selected.chain_name"
         const selectedAddress = this.accounts?.address.find(x => x.chain === chain)
         return selectedAddress?.addr
       }
