57c20a00bc73eca7eca655794df0b5548ba94e68
muhammadp <muhammad.perreira@onfinality.io>
2022-09-08 11:17:02 -0400

    UI changes

--- themeConfig.js
+++ themeConfig.js
@@ -9,7 +9,7 @@ export const $themeBreakpoints = {}
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'PING.PUB', // Will update name in navigation menu (Branding)
+    appName: 'Cosmos Explorer', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
     appLogoImage: '/logos/point-logo.png', // Will update logo in navigation menu (Branding)
   },
