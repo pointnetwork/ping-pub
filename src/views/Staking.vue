78d3e514618abea05e0784bc11d2ab04bc2bb61e
muhammadp <muhammad.perreira@onfinality.io>
2022-09-08 13:48:04 -0400

    minor change

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card
       v-if="pingVals && pingVals.length > 0"
-      title="❤️ Helping Point Network By Staking ❤️"
+      title=""
       class="overflow-auto"
     >
       <b-table
