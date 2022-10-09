d20f2bdaed2d09445d0c11e184167b00e4a2c765
muhammadp <muhammad@pointlabs.org>
2022-10-09 17:23:05 -0400

    test

--- src/views/WalletDelegations.vue
+++ src/views/WalletDelegations.vue
@@ -179,11 +179,8 @@ export default {
       return addr
     },
     findReward(delegator, validator) {
-      const reward = this.reward[delegator].rewards.find(x => x.validator_address === validator) || null
-      if (reward) {
-        return tokenFormatter(reward.reward, this.ibcDenoms)
-      }
-      return '-'
+      const reward = this.rewards[delegator]?.rewards.find(x => x.validator_address === validator)
+      return tokenFormatter(reward.reward, this.ibcDenoms)
     },
     getPrice(denom) {
       const denom2 = 'point-network'
