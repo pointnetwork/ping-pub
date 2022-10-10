ddc66c10898de8f044b0aa93cad2a1e6001dde83
Muhammadp <muhammad@zukamedia.com>
2022-10-10 08:28:14 -0400

    Price fix IBC and Rewards (#9)
    * price changes
    
    * change quotes and denom
    
    * fixes for lint
    
    * Update WalletAccounts.vue
    
    * Update WalletAccounts.vue
    
    * use response object
    
    * check quote
    
    * revert quote changes + update views
    
    * check new price feed
    
    * Update fetch.js and index.js
    
    * changes for accountdetail view
    
    * Update WalletDelegations.vue
    
    * Update fetch.js and WalletDelegations.vue
    
    * use apoint denom instead of uatom
    
    * fixes pingpub denom + ibc
    
    * inflation fix
    
    * fix indentation eslint
    
    * change to ibcDenoms
    
    * revert + remove null in rewards
    
    * fix rewards
    
    * Update point.json and WalletDelegations.vue
    
    * use object instead of array
    
    * revert walletDelegations
    
    Co-authored-by: muhammadp <muhammad@pointlabs.org>

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -268,7 +268,7 @@ export default {
       if (key) {
         const accounts = getLocalAccounts() || {}
         const account = Object.entries(accounts)
-          .map(v => ({ wallet: v[0], address: v[1].address.find(x => x.chain === this.$store.state.chains.selected.chain_name) }))
+          .map(v => ({ wallet: v[0], address: v[1].address.find(x => x.chain === 'point') }))
           .filter(v => v.address)
           .find(x => x.wallet === key)
         if (account) {
@@ -304,15 +304,9 @@ export default {
     })
 
     const conf = this.$http.getSelectedConfig()
-    if (conf.excludes && conf.excludes.indexOf('mint') > -1) {
-      this.inflation = '-'
-    } else {
-      this.$http.getMintingInflation().then(res => {
-        this.inflation = `${percent(res)}%`
-      }).catch(() => {
-        this.inflation = '-'
-      })
-    }
+    this.$http.getMintingInflation().then(res => {
+      this.inflation = `${percent(res)}%`
+    })
 
     this.$http.getGovernanceListByStatus(2).then(res => {
       this.proposals = res.proposals
